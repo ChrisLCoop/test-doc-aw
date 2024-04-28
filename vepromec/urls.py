79110c236f428c2a64a18ebcdc04ec96e1f0a2d3
ChrisLCoop <christian.limas.e@gmail.com>
2024-04-27 20:24:56 -0500

    aaass

--- vepromec/urls.py
+++ vepromec/urls.py
@@ -13,6 +13,8 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 """
 from django.contrib import admin
 from django.urls import path,include
@@ -23,7 +25,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('web.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
